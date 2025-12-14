34d5dd0f985eaa94db45d23c45f5e9c72a622760
LOGPVR <214538967+LOGPVR@users.noreply.github.com>
2025-12-15 00:49:13 +0300

    Ispravil gervuyu i dobavil vtoruyu stroku v main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Это первая строка");
+        System.out.println("Это первая строка в программе");
+        System.out.println("Это вторая строка в программе");
     }
 }
